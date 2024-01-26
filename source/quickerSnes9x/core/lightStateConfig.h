22e3bb231bc934564b0f92d9582e50a8ef87b6a2
Sergio Martin <eienburuu@gmail.com>
2024-01-26 18:00:54 +0100

    Adding missing files

---
+++ source/quickerSnes9x/core/lightStateConfig.h
@@ -0,0 +1,13 @@
+
+
+extern __thread bool _enablePPUBlock;
+extern __thread bool _enableDMABlock;
+extern __thread bool _enableVRABlock;
+extern __thread bool _enableRAMBlock;
+extern __thread bool _enableSRABlock;
+extern __thread bool _enableFILBlock;
+extern __thread bool _enableSNDBlock;
+extern __thread bool _enableCTLBlock;
+extern __thread bool _enableTIMBlock;
+
+extern __thread size_t _SMBBlocks;
